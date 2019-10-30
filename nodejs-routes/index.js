449690734a7b5db52f7cea8b0c3c7a131aaef858
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-30 18:43:36 +0100

    testing fs writeFile method

--- nodejs-routes/index.js
+++ nodejs-routes/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const port = 3000;
 
+let lyrics = 'Hello World';
 
-const port = 3000
+fs.writeFile('URL_logger.txt', lyrics, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('Lyric saved!');
+});
 
 app.get('/', (req, res) => res.send('<h1>Welcome</h1>'));
-app.get('/about-us', (req, res) => res.send('<h1>About us</h1>'))
-app.get('*', (req, res) => res.send('<h1>404</h1>'))
+app.get('/about-us', (req, res) => res.send('<h1>About us</h1>'));
+app.get('*', (req, res) => res.send('<h1>404</h1>'));
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
