6d2213a53555ffff154ae218ed97fe831239f5ec
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-30 19:07:37 +0100

    console loging visited routes

--- nodejs-routes/index.js
+++ nodejs-routes/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const FILE_PATH = 'URL_logger.txt';
 const port = 3000;
 
-let lyrics = 'Hello World';
 
-fs.writeFile('URL_logger.txt', lyrics, (err) => {
-    // throws an error, you could also catch it here
-    if (err) throw err;
+// fs.writeFile('URL_logger.txt', lyrics, (err) => {
+//     if (err) throw err;
+//     console.log('Lyric saved!');
+// });
+// fs.appendFile('URL_logger.txt', '\nRight there up on Broadway', (err) => {
+//     if (err) throw err;
+//     console.log('The lyrics were updated!');
+// });
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`) 
+    next()
+ })
 
-    // success case, the file was saved
-    console.log('Lyric saved!');
+
+app.use('/about-us', function(req, res, next) {
+    next();
 });
 
-app.get('/', (req, res) => res.send('<h1>Welcome</h1>'));
+app.get('/', (req, res) => 
+res.send('<h1>Welcome</h1>')
+);
+
 app.get('/about-us', (req, res) => res.send('<h1>About us</h1>'));
 app.get('*', (req, res) => res.send('<h1>404</h1>'));
 
