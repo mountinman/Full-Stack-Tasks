5b60cff9f850a489381755cbb7707d860d78a2a2
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-30 18:33:59 +0100

    adding routes

--- nodejs-routes/index.js
+++ nodejs-routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('<h1>Welcome</h1>'));
+app.get('/about-us', (req, res) => res.send('<h1>About us</h1>'))
+app.get('*', (req, res) => res.send('<h1>404</h1>'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
