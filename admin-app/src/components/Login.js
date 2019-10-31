ee198384c47b3ff37c185070aa711ec3c35deb2b
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-31 19:59:14 +0100

    adding login form

--- admin-app/src/components/Login.js
+++ admin-app/src/components/Login.js
@@ -2,8 +2,31 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
-      <p>Login</p>
+    <div className="login-page">
+      <h2>Login In</h2>
+      <form autoComplete="off">
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            autoComplete="new-password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
