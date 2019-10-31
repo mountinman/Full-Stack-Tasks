90d611b3f248afbf52a14e1383b6794ea7520c4f
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-31 23:44:23 +0100

    fetching users with axios

--- admin-app/src/components/Users.js
+++ admin-app/src/components/Users.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from "react";
 import NavBar from "./NavBar";
+import axios from "axios";
 
-function Users() {
+class User extends Component {
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
     return (
-        <div>
-            <NavBar></NavBar>
-            <p>Users</p>
-        </div>
+      <div>
+        <NavBar></NavBar>
+        <p>User</p>
+      </div>
     );
+  }
 }
 
-export default Users;
+export default User;
