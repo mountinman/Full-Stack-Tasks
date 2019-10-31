5ef3a9a475bd003bce8a5282c07519a8144b4b51
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-11-01 00:26:37 +0100

    populating users to users page in table form

--- admin-app/src/components/Users.js
+++ admin-app/src/components/Users.js
@@ -3,22 +3,57 @@ import NavBar from "./NavBar";
 import axios from "axios";
 
 class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
   componentDidMount() {
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then(response => {
-        console.log(response.data);
+        this.setState({ users: response.data });
       })
       .catch(error => {
         console.log(error);
       });
   }
 
+  renderTableData() {
+    return this.state.users.map((user, index) => {
+      const { id, name, username, email } = user;
+      return (
+        <tr key={index}>
+          <td>{id}</td>
+          <td>{name}</td>
+          <td>{username}</td>
+          <td>{email}</td>
+        </tr>
+      );
+    });
+  }
+  renderTableHeader() {
+    console.log(this.state.users);
+  }
+
   render() {
     return (
       <div>
         <NavBar></NavBar>
-        <p>User</p>
+        <table id="users">
+          <tbody>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>USERNAME</th>
+              <th>EMAIL</th>
+            </tr>
+            {this.renderTableData()}
+          </tbody>
+        </table>
+        <button onClick={this.renderTableHeader.bind(this)}>Click</button>
       </div>
     );
   }
