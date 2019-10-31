ea9abb3869f96a0d208427a7a40d6d94bd974fab
MarkoCvrljak <marko.cvrljak1@gmail.com>
2019-10-31 22:31:27 +0100

    working on login functionality

--- admin-app/src/components/Login.js
+++ admin-app/src/components/Login.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function Login() {
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const history = useHistory();
+
+  const handleEmailChange = e => {
+    setEmailInput(e.target.value);
+  };
+
+  const handlePasswordChange = e => {
+    setPasswordInput(e.target.value);
+  };
+
+  const handleLoginSubmit = e => {
+    let hardcodedCred = {
+      email: "email@email.com",
+      password: "password123"
+    };
+
+    if (
+      emailInput === hardcodedCred.email &&
+      passwordInput === hardcodedCred.password
+    ) {
+      //combination is good. Log them in.
+      //this token can be anything. You can use random.org to generate a random string;
+      const token = "123456abcdef";
+      sessionStorage.setItem("auth-token", token);
+      history.push("/users");
+    } else {
+      //bad combination
+      alert("wrong email or password combination");
+    }
+  };
   return (
     <div className="login-page">
       <h2>Login In</h2>
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={handleLoginSubmit}>
         <div className="form-group">
           <input
             type="email"
@@ -12,6 +45,8 @@ function Login() {
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            value={emailInput}
+            onChange={handleEmailChange}
           />
         </div>
         <div className="form-group">
@@ -21,6 +56,8 @@ function Login() {
             className="form-control"
             id="exampleInputPassword1"
             placeholder="Password"
+            value={passwordInput}
+            onChange={handlePasswordChange}
           />
         </div>
         <button type="submit" className="btn btn-primary">
